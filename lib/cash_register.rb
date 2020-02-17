a1a6551dc94528e24b06e99e66cede9712138f0e
Timur Mishiev <timurmishiev@gmail.com>
2020-02-17 06:47:13 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,12 +7,9 @@ class CashRegister
     @discount = discount
     @items = []
   end
-  
-  
-  #accepts a title and a price and increases the total
-   #also accepts an optional quantity
+
+
   def add_item(title,price,quantity = 1)
-    
     if quantity>1
       i=0
       while i<quantity
@@ -22,27 +19,26 @@ class CashRegister
     else
       @items << title
     end
-    #doesn't forget about the previous total
     @total += price*quantity
     @last_transaction_amount = @total
     @total
   end
-  
+
   def apply_discount()
-     #applies the discount to the total price
-      if @discount > 0
+    if @discount > 0
       @discount = @discount/100.to_f
-      #reduces the total
       @total = @total - (@total * (@discount))
-      #returns success message with updated total
-       "After the discount, the total comes to $#{@total.to_i}."
-      else
+      "After the discount, the total comes to $#{@total.to_i}."
+    else
       "There is no discount to apply."
     end
   end
-  
-  
-   def void_last_transaction()
-    @total = @total - last_transaction_amount
-   end 
+
+
+  def void_last_transaction()
+    @total -= @last_transaction_amount
+  end
+
+end
+
   end 
