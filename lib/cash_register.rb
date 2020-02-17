30335fe5922421d9aa323db855fa5f4310a314fe
Timur Mishiev <timurmishiev@gmail.com>
2020-02-17 06:30:17 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,19 +8,6 @@ class CashRegister
     @items = []
   end
  
-  def add_item(title, price, quantity = 1)
-    if quantity > 1
-      i = 0
-      while i < quantity 
-      @item << title
-      i+=1
-  end 
-  else 
-  @items << ttle 
-  end 
-  @total += price*quantity
-    @last_transaction_amount = @total
-    @total
-  end 
+  
   
 end 
